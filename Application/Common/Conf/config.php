05e45cc537dd1d7f4e204b0aa28d978e5ee5054c
talerwen <602943077@qq.com>
2017-03-16 08:48:03 +0800

    first commit

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+	// '配置项'=>'配置值'
+	// 配置数据库
+	'DB_TYPE' => 'mysql',
+	'DB_HOST' => '127.0.0.1',
+	'DB_NAME' => 'shop',
+	'DB_USER' => 'root',
+	'DB_PWD'  => '123456',
+	'DB_PREFIX' => 'sh_',
+	// 配置默认模块
+	'DEFAULT_MODULE' => 'Admin',
+);
